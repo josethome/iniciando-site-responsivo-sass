6baf1a36e581bbde9247c6d857af53b95cc7dd20
Jose Thome <josethome.band@hotmail.com>
2019-12-11 16:21:28 -0200

    adicionado menu dinâmico ao projeto

--- wp-content/themes/portfolio-theme/functions.php
+++ wp-content/themes/portfolio-theme/functions.php
@@ -11,4 +11,22 @@
 		);		
 	}	
 	add_action('init', 'register_my_menus');
+
+	// MEUS POSTS TYPES
+	function meus_posts_types() {
+		// HABILIDADES
+		register_post_type('habilidades',
+			array(
+				'labels'			=> array(
+					'name'			=> __('Habilidades'),
+					'singular_name' =>	__('Habilidade')
+					),
+				'public'			=> true,
+				'has_archive'		=> true,
+				'menu_icon'			=> 'dashicons-welcome-learn-more',
+				'supports'			=>	array('title', 'thumbnail', 'page-attributes'),
+				)
+		);
+	}
+	add_action('init', 'meus_posts_types');
 ?>
